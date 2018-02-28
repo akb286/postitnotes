172cad642811156abaebf167a42cb7f36b56b522
Alicia Baker <aliciabaker314@gmail.com>
2018-02-27 17:11:02 -0700

    used fakers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'notes#home'
+
+  get '/about', to: 'notes#about'
+
+  resources :notes
 end
